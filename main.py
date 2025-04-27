65c6b6acef26ec1e9f5afe84af6d1eb2dcde982d
Oscar Holt <oscar.holt99@gmail.com>
2025-04-27 17:44:53 +1200

    create constants, set up game loop, update .gitignore

--- main.py
+++ main.py
@@ -2,9 +2,18 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
